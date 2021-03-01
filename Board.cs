f9b11b8727b64a8c2e0cc4e31286dd06ec3a4c27
ajana4096 <41270954+ajana4096@users.noreply.github.com>
2021-03-01 11:31:12 +0100

    Fix ships setup

---
+++ Board.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Board
+    {       
+        private List<Ship> ships;
+        int ships_count;
+        private short[,] square_states;//logical states of squares: -1 - already checked by the opponent, 0 - empty, greater then 0 - occupied by a ship with id one less than the state 
+        public Board(int size, List<int> ships)//create ship of given size and number of ships
+        {
+            square_states = new short[size, size];
+            this.ships = new List<Ship>(ships.Count);
+            this.ships_count = ships.Count;
+            Random rm = new Random((int)DateTime.Now.Ticks);            
+            //the code doesn't detect that more ships cannot be placed since it's beyond scope of the question 
+            //and number of ships flexibility is added only because I consider it good practice
+            for(int i=0;i<ships.Count;i++)
+            {
+                bool correct_position = false;
+                this.ships.Add(new Ship(ships[i]));
+                int direction,x,y;
+                do
+                {
+                    direction = rm.Next(0, 2); //choose if ship is placed horizontally(0) or vertically(y)
+                    correct_position = true;
+                    //check if the position is free
+                    if (direction == 1)///horizontal
+                    {
+                        //choose the position of the top left square of the new ship
+                        x = rm.Next(0, size-ships[i]);
+                        y = rm.Next(0, size);
+
+                        for (int k = x; k < x + ships[i]; k++)
+                        {
+                            if (square_states[k, y] != 0)
+                            {
+                                correct_position = false;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //choose the position of the top left square of the new ship
+                        x = rm.Next(0, size);
+                        y = rm.Next(0, size-ships[i]);
+
+                        for (int k = y; k < y + ships[i]; k++)
+                        {
+                            if (square_states[x, k] != 0)
+                            {
+                                correct_position = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                while (!correct_position);//repest until you find correct position
+                //save the ships position
+                if (direction == 1)
+                {
+                    for (int k = x; k < x + ships[i]; k++)
+                    {
+                        square_states[k, y] = (short)(i + 1);
+                        this.ships[i].add_square(k, y);
+                    }
+                }
+                else
+                {
+                    for (int k = y; k < y + ships[i]; k++)
+                    {
+                        square_states[x, k] = (short)(i + 1);
+                        this.ships[i].add_square(x,k);
+                    }
+                }
+            }
+        }
+        //execute player's move and return result
+        public int shoot(int x, int y)
+        {
+            int result = square_states[x, y];
+            if(square_states[x,y]>0)//check if a ship was hit
+            {
+                result = 1;                
+                if(this.ships[square_states[x, y]-1].shoot())//check if the ship was sunk
+                {
+                    ships_count--;
+                    result++;                    
+                    if(ships_count==0)//check if there are ships remaining
+                    {                        
+                        result++;
+                    }
+                }
+            }            
+            return result;
+        }
+        public void mark_as_checked(int x, int y)
+        {
+            square_states[x, y] = -1;
+        }
+        //returns all squares of a ship that occupies a given square
+        public List<KeyValuePair<int,int>>get_ship_from_field(int x, int y)
+        {
+            return this.ships[square_states[x, y] - 1].get_squares();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleship
+{
+    public class BattleshipButton : System.Windows.Forms.Button
+    {
+        private int x,y;        
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public BattleshipButton(int x, int y, EventHandler eventHandler)
+        {
+            this.x = x;
+            this.y = y;
+            this.Click += eventHandler;
+            this.Size = new System.Drawing.Size(50, 50);
+            this.Location = new System.Drawing.Point(x*50, (y+1)*50);
+            this.UseVisualStyleBackColor = true;            
+            this.Name = string.Format("{0}x{1}",x,y);            
+            this.TabIndex = 0;
+            this.BackgroundImage = Properties.Resources.sea;
+        }
+    }
+    public partial class Form1 : Form
+    {        
+        private Board board;
+        Image empty, hit, miss, sunk;
+        public Form1()//start game with default parameteres - 10x10 and three ships one 5-square and two 4-square
+        {
+            List<int> ships = new List<int> { 5, 4, 4 };
+            setup_objects(10, ships);
+
+        }
+        public Form1(int size, List<int>ships)//start game with defined parameteres
+        {
+            setup_objects(size, ships);
+        }
+        private void setup_objects(int size, List<int>ships)
+        {
+            board = new Board(size, ships);
+            miss = Properties.Resources.miss;
+            hit = Properties.Resources.hit;
+            sunk = Properties.Resources.sunk;
+            InitializeComponent();
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    this.Controls.Add(new BattleshipButton(x, y, shoot_handler));
+                }
+            }
+        }
+        public void shoot_handler(object sender, EventArgs e)
+        {
+            BattleshipButton button = (BattleshipButton)sender;
+            int result = board.shoot(button.getX(), button.getY());
+            switch(result)
+            {
+                case -1:
+                    
+                    break;
+                case 0:
+                    button.BackgroundImage = miss;
+                    break;
+                case 1:
+                    button.BackgroundImage = hit;
+                    break;
+                case 2:
+                    List<KeyValuePair<int,int>>sunk_ship = board.get_ship_from_field(button.getX(), button.getY());
+                    foreach(KeyValuePair<int,int>square in sunk_ship)
+                    {
+                        ((BattleshipButton)this.Controls.Find(string.Format("{0}x{1}", square.Key, square.Value), false)[0]).BackgroundImage = sunk;
+                    }
+                    break;
+                case 3:
+                    sunk_ship = board.get_ship_from_field(button.getX(), button.getY());
+                    foreach (KeyValuePair<int, int> square in sunk_ship)
+                    {
+                        ((BattleshipButton)this.Controls.Find(string.Format("{0}x{1}", square.Key, square.Value), false)[0]).BackgroundImage = sunk;
+                    }
+                    break;
+            }
+            board.mark_as_checked(button.getX(), button.getY());
+        }
+    }
+}
