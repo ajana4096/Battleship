97fd4d1b9a832ae2d60d4a458128b9c0f98f1947
ajana4096 <41270954+ajana4096@users.noreply.github.com>
2021-03-01 10:38:34 +0100

    First draft

---
+++ Battleship/Board.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Board
+    {       
+        private List<int> ships;
+        int ships_count;
+        private short[,] square_states;//logical states of squares: -1 - already checked by the opponent, 0 - empty, greater then 0 - occupied by a ship with id one less than the state 
+        public Board(int size, List<int> ships)//create ship of given size and number of ships
+        {
+            square_states = new short[size, size];
+            this.ships = ships;
+            this.ships_count = ships.Count;
+            Random rm = new Random((int)DateTime.Now.Ticks);            
+            //the code doesn't detect that more ships cannot be placed since it's beyond scope of the question 
+            //and number of ships flexibility is added only because I consider it good practice
+            for(int i=0;i<ships.Count;i++)
+            {
+                bool correct_position = false;
+                int direction,x,y;
+                do
+                {
+                    direction = rm.Next(0, 1); //choose if ship is placed horizontally(0) or vertically(y)
+                    correct_position = true;
+                    //check if the position is free
+                    if (direction == 0)///horizontal
+                    {
+                        //choose the position of the top left square of the new ship
+                        x = rm.Next(0, size-ships[i]);
+                        y = rm.Next(0, size);
+
+                        for (int k = x; k < x + ships[i]; k++)
+                        {
+                            if (square_states[k, y] != 0)
+                            {
+                                correct_position = false;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        //choose the position of the top left square of the new ship
+                        x = rm.Next(0, size);
+                        y = rm.Next(0, size-ships[i]);
+
+                        for (int k = y; k < y + ships[i]; k++)
+                        {
+                            if (square_states[x, k] != 0)
+                            {
+                                correct_position = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                while (!correct_position);//repest until you find correct position
+                //save the ships position
+                if (direction == 0)
+                {
+                    for (int k = x; k < x + ships[i]; k++)
+                    {
+                        square_states[k, y] = (short)(i + 1);
+                    }
+                }
+                else
+                {
+                    for (int k = y; k < y + ships[i]; k++)
+                    {
+                        square_states[x, k] = (short)(i + 1);
+                    }
+                }
+            }
+        }
+        //execute player's move and return result
+        public int shoot(int x, int y)
+        {
+            int result = square_states[x, y];
+            if(square_states[x,y]>0)//check if a ship was hit
+            {
+                result = 1;
+                ships[square_states[x, y] - 1]--;
+                if(ships[square_states[x, y] - 1]==0)//check if the ship was sunk
+                {
+                    result++;
+                    ships_count--;
+                    if(ships_count==0)//check if there are ships remaining
+                    {
+                        result++;
+                    }
+                }
+            }
+            square_states[x, y] = -1;
+            return result;
+        }
+    }
+}
---
+++ Battleship/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleship
+{
+    public class BattleshipButton : System.Windows.Forms.Button
+    {
+        private int x,y;        
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public BattleshipButton(int x, int y, EventHandler eventHandler)
+        {
+            this.x = x;
+            this.y = y;
+            this.Click += eventHandler;
+            this.Size = new System.Drawing.Size(50, 50);
+            this.Location = new System.Drawing.Point(x*50, (y+1)*50);
+            this.UseVisualStyleBackColor = true;            
+            this.Name = string.Format("{0}x{1}",x,y);            
+            this.TabIndex = 0;
+        }
+    }
+    public partial class Form1 : Form
+    {        
+        private Board board;
+        Image empty, hit, miss, sunk;
+        public Form1()//start game with default parameteres - 10x10 and three ships one 5-square and two 4-square
+        {
+            List<int> ships = new List<int> { 5, 4, 4 };
+            setup_objects(10, ships);
+
+        }
+        public Form1(int size, List<int>ships)//start game with defined parameteres
+        {
+            setup_objects(size, ships);
+        }
+        private void setup_objects(int size, List<int>ships)
+        {
+            board = new Board(size, ships);
+            InitializeComponent();
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    this.Controls.Add(new BattleshipButton(x, y, shoot_handler));
+                }
+            }
+            empty = Properties.Resources.sea;
+            miss = Properties.Resources.miss;
+            hit = Properties.Resources.hit;
+            sunk = Properties.Resources.sink;
+        }
+        public void shoot_handler(object sender, EventArgs e)
+        {
+            BattleshipButton button = (BattleshipButton)sender;
+            int result = board.shoot(button.getX(), button.getY());
+            switch(result)
+            {
+                case 0:
+                    
+                    break;
+            }
+        }
+    }
+}
